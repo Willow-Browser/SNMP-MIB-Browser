390ffe58d29c6f9b0254752ae8ddf05d482547b3
Gabriel Fidder <gabfid@gmail.com>
2022-07-13 00:31:56 -0500

    Adding some parsing logic

--- MIB_Browser/objects/oid_storage.py
+++ MIB_Browser/objects/oid_storage.py
@@ -1,4 +1,16 @@
 from PyQt5.QtGui import QStandardItem, QColor, QIcon
+from enum import Enum
+
+
+class OID_Type(Enum):
+    MODULE_IDENTITY = 1
+    TEXTUAL_CONVENTION = 2
+    OBJECT_IDENTITY = 3
+    OBJECT_TYPE = 4
+    NOTIFICATION_TYPE = 5
+    MODULE_COMPLIANCE = 6
+    OBJECT_GROUP = 7
+    NOTIFICATION_GROUP = 8
 
 
 class OID_Object(QStandardItem):
--- MIB_Browser/scripts/parse_mibs.py
+++ MIB_Browser/scripts/parse_mibs.py
@@ -1,7 +1,6 @@
 import imp
 import os
 import sys
-from this import d
 from PyQt5.QtWidgets import QTreeView
 from pysmi.reader import getReadersFromUrls
 from pysmi.searcher import AnyFileSearcher, PyFileSearcher, PyPackageSearcher, StubSearcher
@@ -17,25 +16,22 @@ from pysmi import debug
 from pysmi import error
 import json
 
+from MIB_Browser.objects.oid_storage import OID_Object
+
 data = {}
+classes = []
 
 
 def parse_mibs(mib: str, tree_view: QTreeView):
     head_tail = os.path.split(mib)
     code_gen = JsonCodeGen()
-    debug.setLogger(debug.Debug('compiler'))
+    # debug.setLogger(debug.Debug('compiler'))
 
     mib_stubs = JsonCodeGen.baseMibs
 
     dstDirectory = os.path.join('.')
 
-    mibBorrowers = [('http://mibs.snmplabs.com/json/notexts/@mib@', False),
-                    ('http://mibs.snmplabs.com/json/fulltexts/@mib@', True)]
-
-    borrowers = [AnyFileBorrower(x[1], genTexts=mibBorrowers[x[0]][1]).setOptions(exts=['.json'])
-                 for x in enumerate(getReadersFromUrls(*[m[0] for m in mibBorrowers], **dict(lowcaseMatching=False)))]
-
-    searchers = [AnyFileSearcher(dstDirectory).setOptions(exts=['.json']), StubSearcher(*mib_stubs)]
+    searchers = [StubSearcher(*mib_stubs)]
 
     fileWriter = CallbackWriter(func)
 
@@ -52,20 +48,29 @@ def parse_mibs(mib: str, tree_view: QTreeView):
     mib_name, _ = os.path.splitext(head_tail[1])
 
     try:
-        mib_compiler.addSources(FileReader(head_tail[0]))
+        mib_compiler.addSources(
+            FileReader(head_tail[0]).setOptions(**dict(fuzzyMatching=True)))
+
+        mib_compiler.addSources(FileReader(
+            os.path.join(os.path.expanduser("~"), ".pysnmp", "mibs")))
 
         mib_compiler.addSearchers(*searchers)
 
-        mib_compiler.addBorrowers(*borrowers)
+        # TODO : actually write the mib parsing logic
 
         _ = mib_compiler.compile(
             mib_name,
-            **dict(noDeps=True,
+            **dict(noDeps=False,
                    genTexts=True,
                    textFilter=True and (lambda symbol, text: text) or None))
+
+        xyz = 1
     except error.PySmiError:
         sys.stderr.write('ERROR: %s\r\n' % sys.exc_info()[1])
         sys.exit(70)
+
+    for some_class in classes:
+        print(some_class)
 
     pass
 
@@ -78,6 +83,7 @@ def func(mibname, contents, cbCtx):
 class TestFileWrite(AbstractWriter):
     def __init__(self, path) -> None:
         self._path = decode(os.path.normpath(path))
+        self.oids = []
 
     def __str__(self) -> str:
         return '%s{"%s"}' % (self.__class__.__name__, self._path)
@@ -100,7 +106,27 @@ class TestFileWrite(AbstractWriter):
     def putData(self, mibname, data, comments=(), dryRun=False):
         newData = json.loads(data)
 
-        # newData is a dictionary that contains what we should need
-
-        x = 1
+        for key, y in newData.items():
+            if key != 'imports':
+                z = 1
+                try:
+                    if y['class'] not in classes:
+                        classes.append(y['class'])
+                    if y['class'] == 'moduleidentity':
+                        oid = OID_Object(y['name'], y['oid'], mibname)
+                        oid.description = y['description']
+                        self.oids.append(oid)
+                    elif y['class'] == 'objectidentity':
+                        oid = OID_Object(y['name'], y['oid'], mibname)
+                        if 'description' in y:
+                            oid.description = y['description']
+                        self.oids.append(oid)
+                    elif y['class'] == 'objecttype':
+                        oid = OID_Object(y['name'], y['oid'], mibname)
+                        oid.description = y['description']
+                        oid.access = y['maxaccess']
+                        oid.status = y['status']
+                        self.oids.append(oid)
+                except KeyError:
+                    xy = 5
         return
